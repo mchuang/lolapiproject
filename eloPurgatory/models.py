9b36135178db0c726b387693bd36ae92d710a15b
Michael Chuang <Michael Chuang>
2016-12-06 14:51:31 -0800

    Added basic models and more view calls

--- eloPurgatory/models.py
+++ eloPurgatory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Summoner(models.Model):
+    summonerId = models.BigIntegerField(primary_key=True)
+    name = models.CharField()
+    profileIcon = models.IntegerField()
+
+class RankInfo(models.Model):
+    summonerId = models.ForeignKey(Summoner, on_delete=models.CASCAFE, verbose_name="Summoner ranks",)
+    queue = models.CharField()
+    lp = models.IntegerField()
+    tier = models.CharField()
+    division = models.CharField()
+
--- eloPurgatory/views.py
+++ eloPurgatory/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 import requests
 import time
 
-
 RATE_LIMIT_EXCEEDED_SLEEP_DURATION = 10
 
 api_key='RGAPI-fb6382dc-b504-49fb-8361-54dec10d386b'
@@ -17,7 +16,7 @@ URLS = dict(
     recent_games='v1.3/game/by-summoner/{summonerId}/recent',
     ranked_stats='v1.3/stats/by-summoner/{summonerId}/ranked',
     summary_stats='v1.3/stats/by-summoner/{summonerId}/summary',
-    league_data='v2.5/league/by-summoner/{summonerId}'
+    rank_data='v2.5/league/by-summoner/{summonerId}/entry'
 )
 
 from django.http import HttpResponse
@@ -27,18 +26,30 @@ def basicCall(request, region, summonerName):
     return HttpResponse(r)
 
 
-def executeRequest(url):
-    payload = {'api_key': api_key}
-    r = requests.get(url, params=payload)
-    return r
+def executeRequest(url, payload):
+    response = requests.get(url, params=payload)
+    while response.status_code == requests.codes.too_many_requests:
+        time.sleep(int(r.headers.get('Retry-After')))
+        response = requests.get(url, params=payload)    
+    return response
 
 def getSummonerInfo(region, summonerName):
     url = (base_url+URLS['summoner_by_name']).format(region=region, summonerName=summonerName)
-    r = executeRequest(url)
-    if handle_status(r):
-        return r
-    else:
-        return null
+    payload = {'api_key': api_key}
+    response = executeRequest(url, payload)
+    return response
+
+def getRankInfo(region, summonerId):
+    url = (base_url+URLS['rank_data'].format(region=region, summonerId=summonerId))
+    payload = {'api_key': api_key}
+    response = executeRequest(url, payload)
+    return response
+
+def getMatchInfo(summonderId):
+    url = (base_url+URLS['match_list'].format(summonerId=summonerId))
+    payload = {'api_key': api_key}
+    response = executeRequest(url, payload)
+    return response
 
 def handle_status(r):
     if r.status_code == requests.codes.ok:
